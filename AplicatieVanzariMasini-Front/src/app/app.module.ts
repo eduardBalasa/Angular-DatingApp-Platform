f041f3e9a1474bd1a960679c3d28bbb5e6a4a80b
Eduard_MB <edibmarian@gmail.com>
2020-03-11 22:45:16 +0200

    Adding Ngx Bootstrap, Angular JWT and wrapping libraries in Angular service

--- AplicatieVanzariMasini-Front/src/app/app.module.ts
+++ AplicatieVanzariMasini-Front/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
    declarations: [
@@ -20,7 +22,9 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
